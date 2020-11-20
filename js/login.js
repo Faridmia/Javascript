82c2975f505f54181079dfcb16f454737bb4f9f2
faridmia <faridcse7800@gmail.com>
2020-11-20 17:24:29 +0600

    logical ifelse

---
+++ js/login.js
@@ -0,0 +1,13 @@
+var n = 15;
+
+// if(n < 10){
+//     console.log("This number",n, "is smaller than 10");
+// }else{
+//     console.log("This number",n, "is greater than 10");
+// }
+
+if(11 ==n){
+    console.log("n is equal to 11");
+}else{
+    console.log("n is not equal to 11");
+}
