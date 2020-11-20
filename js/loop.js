5f1290e7a70adfa9ebe9b882753496ae44352c99
faridmia <faridcse7800@gmail.com>
2020-11-20 20:12:19 +0600

    while loop

---
+++ js/loop.js
@@ -0,0 +1,27 @@
+// var i = 10;
+
+// while(i-- > 0){
+//     console.log(i);
+// }
+
+var i =0;
+
+// while(i<=10){
+    
+
+//     console.log(i);
+//     i++;
+// }
+
+// while(i<=10){
+//     console.log(i);
+//     i+=2;
+// }
+
+while(true){
+    console.log(i);
+    if(10 == i){
+        break;
+    }
+    i++;
+}
