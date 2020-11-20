aa0763afe4cf81520d594729ddebf4acf3647d06
faridmia <faridcse7800@gmail.com>
2020-11-20 21:46:34 +0600

    factorial for loop

--- js/assignment.js
+++ js/assignment.js
@@ -5,3 +5,8 @@ console.log(x);
 
 y = 4;
 y -=2;
+
+// for(i = n;i>1;i--){
+//     factorial *=i; 
+// }
+// console.log("Factorial Number:",factorial);
---
+++ js/for-loop-factorical.js
@@ -0,0 +1,23 @@
+var n = 6;
+var factorial = 1;
+
+// for(i = n;i>1;i--){
+//     factorial *=i; 
+// }
+// console.log("Factorial Number:",factorial);
+
+// for(j = 1;j<=10;j++){
+//     factorial=1;
+
+//     for(var i =j;i>1;i--){
+//         factorial*=i;
+//     }
+
+//     console.log("factorial of : ", j, "is ", factorial);
+// }
+
+var factorial=1;
+for(i = 1;i<=10;i++){
+    factorial*=i;
+    console.log("factorial of : ", i, "is ", factorial);
+}
