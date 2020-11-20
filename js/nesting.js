db2e3b18e2a54e8e79b55d06bd68b9d389ac3577
faridmia <faridcse7800@gmail.com>
2020-11-20 17:45:10 +0600

    nesting js condition

---
+++ js/nesting.js
@@ -0,0 +1,9 @@
+n =20;
+
+if(n<10 && ( 2==n || 3==n || 5==n || 7==n)){
+    console.log("this number",n,"is smaller than 10 and prime number");
+}else if(n<10){
+    console.log("this number",n,"is smaller than 10 and is not a prime number");
+}else{
+    console.log("this number",n,"is grater than 10");
+}
