239e1aaddcedc813e6a0196d0aa78b12fe7cf6c5
faridmia <faridcse7800@gmail.com>
2020-11-20 16:00:38 +0600

    javascript arithmatic operator

---
+++ js/variable-01.js
@@ -0,0 +1,57 @@
+// var n1 = 5;
+// var r1 = 5 % 2;
+// console.log(r1);
+
+// operator prefix and postfix
+var x;
+
+var y = 5;
+
+//var x = x++;
+//atar  mane holo
+// x = y;
+// y = y+1;
+
+x = ++y;
+
+//atar mane 
+y = y+1;
+x = y;
+
+
+console.log("x =",x ,"y = ",y);
+
+
+
+// x++;
+// console.log(x);
+
+
+
+
+
+
+
+
+// eggPrice = 10;
+// needEggs = 12;
+
+// totalEggs = eggPrice * needEggs;
+// console.log(totalEggs);
+
+// var radius = 7;
+// pi = 3.1416;
+
+// var areaOfcircle = pi * (radius * radius);
+
+// console.log(areaOfcircle);
+
+
+
+// var myName = "farid mia";
+// var myAge = 25;
+// console.log("My name is",myName,"I am",myAge,"years old");
+
+// const pi = 3.1416;
+
+// console.log(pi);
