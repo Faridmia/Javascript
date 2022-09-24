d0ced1c0208531a940875379f41e40ce6f1d9de9
faridmia <mdfarid7830@gmail.com>
2022-09-24 22:38:11 +0600

    callback api array

---
+++ javascript_es6/callback_and_promise.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+    let list = [];
+    $.get('https://jsonplaceholder.typicode.com/posts', ( response ) => {
+
+        console.log(response);
+
+        const id = 1;
+
+        $.get(`https://jsonplaceholder.typicode.com/posts/${id}`, (response) => {
+
+            console.log(response);
+
+            $.get(`https://jsonplaceholder.typicode.com/posts/1/comments`, (response) => {
+                console.log(response);
+            }).fail(err => {
+                console.log(err);
+            })
+        }).fail(err => {
+            console.log(err);
+        })
+
+    }).fail(err => {
+        console.log(err);
+    })
+
+}(jQuery));
