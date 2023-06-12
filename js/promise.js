7587e2e5af5b0cdcbad84e379d5d9320ed359c70
faridmia <mdfarid7830@gmail.com>
2023-06-13 00:30:44 +0600

    javascript  open

--- js/promise.js
+++ js/promise.js
@@ -27,14 +27,43 @@
 
 // },3000);
 
-const myPromise = new Promise(function( myResolve,myReject ) {
-    setTimeout(function () {
-        myResolve("promise function call");   
-    }, 3000 );
+// const myPromise = new Promise(function( myResolve,myReject ) {
+//     setTimeout(function () {
+//         myResolve("promise function call");   
+//     }, 3000 );
+// });
+
+// myPromise.then(
+//     function( value ) {
+//         document.getElementById("demo").innerHTML = value;
+//     }
+// );
+
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+}
+
+
+let myPromise = new Promise (function( myResolve,myReject ) {
+    let req = new XMLHttpRequest();
+    req.open("GET","my_car.html");
+
+    req.onload = function() {
+        if(req.status == 200) {
+            myResolve(req.response);
+        } else {
+           myReject("file not found"); 
+        }
+    };
+    req.send();
 });
 
 myPromise.then(
-    function( value ) {
-        document.getElementById("demo").innerHTML = value;
+    function(value) {
+        myDisplayer(value);
+    },
+    function(error) {
+        myDisplayer(error);
     }
+
 );
