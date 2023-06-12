7f4f1a7154dd1d6ce2c2738f4676f003f23b2550
faridmia <mdfarid7830@gmail.com>
2023-06-12 23:42:21 +0600

    promise callback

--- js/promise.js
+++ js/promise.js
@@ -1,19 +1,28 @@
-function myDisplayer(some) {
-    document.getElementById("demo").innerHTML = some;
+// function myDisplayer(some) {
+//     document.getElementById("demo").innerHTML = some;
+// }
+
+
+// let myPromise = new Promise( function ( myResolve, myReject  ) {
+//     let x = 0;
+
+//     if( x == 0 ) {
+//         myResolve("ok");
+//     } else {
+//         myReject("Error");
+//     }
+// });
+
+// myPromise.then(
+//     function(value) { myDisplayer(value);},
+//     function(error) { myDisplayer(error);},
+// );
+
+function myFunction(value) {
+    document.getElementById("demo").innerHTML = value;
 }
 
+setTimeout(function() {
+    myFunction("time funciton data call");
 
-let myPromise = new Promise( function ( myResolve, myReject  ) {
-    let x = 0;
-
-    if( x == 0 ) {
-        myResolve("ok");
-    } else {
-        myReject("Error");
-    }
-});
-
-myPromise.then(
-    function(value) { myDisplayer(value);},
-    function(error) { myDisplayer(error);},
-);
+},3000);
