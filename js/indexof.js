85cb532aaef8b7e02d2be15b9e4ba11acda5e793
Farid Mia <mdfarid7830@gmail.com>
2023-12-28 22:37:15 +0600

    lastindexof

--- js/indexof.js
+++ js/indexof.js
@@ -1,4 +1,7 @@
 let text = "Please locate where 'locate' occurs!";  // the indexof method return the index positon
 let index = text.indexOf("locate");
 
-console.log(index);
+let text2 = "Please locate where 'Please' occurs!";
+let index2 = text.lastIndexOf("locate");
+
+console.log(index2);
