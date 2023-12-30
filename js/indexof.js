f070981d53aaf7c31bed0391015bbcad37741b3f
Farid Mia <mdfarid7830@gmail.com>
2023-12-30 20:20:36 +0600

    floting number

--- js/indexof.js
+++ js/indexof.js
@@ -2,6 +2,9 @@ let text = "Please locate where 'locate' occurs!";  // the indexof method return
 let index = text.indexOf("locate");
 
 let text2 = "Please locate where 'Please' occurs!";
-let index2 = text.lastIndexOf("locate");
+let index2 = text2.lastIndexOf("locate");
 
-console.log(index2);
+let text3 = "Please locate where 'locate' occurs!";
+let index3 = text3.lastIndexOf("locate", 15);
+
+console.log(index3);
