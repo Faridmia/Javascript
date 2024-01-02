cb03aab3281c87a4b081b440bdfa5ada2c779c18
Farid Mia <mdfarid7830@gmail.com>
2024-01-02 09:12:01 +0600

    array

--- js/array.js
+++ js/array.js
@@ -5,7 +5,25 @@ const fruits2 = ["Banana", "Orange", "Apple"];
 document.getElementById("demo8").innerHTML = fruits2;
 
 function myFunction() {
-    fruits2.push("juice");
+    //fruits2.push("juice");
+    fruits2[fruits2.length] = "juice";
 
     document.getElementById("demo8").innerHTML = fruits2;
 }
+
+
+const fruits4 = ["Banana", "Orange", "Apple"];
+fruits4[6] = "Lemon";  // Creates undefined "holes" in fruits
+
+
+flength = fruits4.length;
+
+let text3 = "<ul>";
+
+for (let m = 0;m<flength;m++) {
+    text3 += "<li>" + fruits4[m] + "</li>";
+}
+
+text3 += '</ul>';
+
+document.getElementById("demo9").innerHTML = text3;
