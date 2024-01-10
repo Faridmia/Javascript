a68f1d406ad29a622c142db6eb0d895a1961c5b7
Farid Mia <mdfarid7830@gmail.com>
2024-01-10 08:14:34 +0600

    filter method array

--- js/array-iteration.js
+++ js/array-iteration.js
@@ -23,3 +23,12 @@ const myArr = [1, 2, 3, 4, 5,6];
 const newArray = myArr.flatMap( (x) => x * 2 );
 
 console.log(newArray);
+
+const numbers2 = [45, 4, 9, 16, 25];
+const over18 = numbers2.filter(myFunction2);
+
+function myFunction2(value,index,array) {
+    return value > 18;
+}
+
+console.log(over18);
