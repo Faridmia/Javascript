fa6db27d7055e221f4830eb7ec27311e01fc9959
faridmia <mdfarid7830@gmail.com>
2024-01-10 23:37:44 +0600

    random function

---
+++ js/math.js
@@ -0,0 +1,15 @@
+let math1 = Math.round(4.6); // return the nearest interger
+
+console.log(math1);
+
+let math2 = Math.ceil(4.9);
+Math.ceil(4.7);
+Math.ceil(4.4);
+Math.ceil(4.2);
+Math.ceil(-4.2);
+
+console.log(math2);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+console.log(random);
