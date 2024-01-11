30f086cbbd471de265378b77c46af2353943c47a
faridmia <mdfarid7830@gmail.com>
2024-01-11 22:58:34 +0600

    max min number

--- js/random.js
+++ js/random.js
@@ -9,3 +9,26 @@ points.sort(function(a,b) {
 })
 
 console.log( points );
+
+const points2 = [40, 100, 1, 5, 25, 10];
+points2.sort(function(a, b){return a - b});
+
+let minvalue = points2[0];
+let maxvalue = points2[ points2.length -1 ];
+
+console.log( minvalue );
+console.log( maxvalue );
+
+
+const points3 = [40, 100, 1, 5, 25, 10];
+
+function myArrayMin(arr) {
+  return Math.min.apply(null, arr);
+}
+
+function myArrayMax(arr) {
+    return Math.max.apply(null, arr);
+  }
+
+console.log( myArrayMin(points3));
+console.log( myArrayMax(points3));
