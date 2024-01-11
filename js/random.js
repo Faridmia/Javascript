65096696e05392f7a63cdad3419c6fbad1afd4b5
faridmia <mdfarid7830@gmail.com>
2024-01-11 22:39:29 +0600

    sort method

--- js/random.js
+++ js/random.js
@@ -1,3 +1,11 @@
 let random_value = Math.floor(Math.random() + 1 );
 
 console.log( random_value );
+
+const points = [40, 100, 1, 5, 25, 10];
+
+points.sort(function(a,b) {
+    return a - b;
+})
+
+console.log( points );
