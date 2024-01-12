3d57f21623ebeb40d0c50af5c529d73ff2731f7b
faridmia <mdfarid7830@gmail.com>
2024-01-12 22:21:18 +0600

    compaing object

--- js/boolean.js
+++ js/boolean.js
@@ -1,6 +1,6 @@
 const x1 = new Boolean(false);
 const y1 = new Boolean(false);
 
-const z1 = x1 == y1;
+const z1 = x1 == y1;  /// Comparing two JavaScript objects always return false.
 
 console.log(z1);
