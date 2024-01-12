8a38edf6020e8c4eedee80a1e0cfc676d63bff42
faridmia <mdfarid7830@gmail.com>
2024-01-12 22:48:33 +0600

    boolean data

--- js/boolean.js
+++ js/boolean.js
@@ -4,3 +4,7 @@ const y1 = new Boolean(false);
 const z1 = x1 == y1;  /// Comparing two JavaScript objects always return false.
 
 console.log(z1);
+
+let booldata = "2" > "12";
+
+console.log(booldata);
