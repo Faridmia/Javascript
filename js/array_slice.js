7040b88f7a3bcb915e731c129cce22aecd879cd9
faridmia <mdfarid7830@gmail.com>
2024-01-13 12:20:31 +0600

    array slice method

---
+++ js/array_slice.js
@@ -0,0 +1,8 @@
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+
+console.log(animals.slice(2)); // 2 number position thake start hobe and element gula show korbe anothers gula remove hobe
+
+console.log(animals.slice(2,4)); // camel,duck 
+console.log(animals.slice(-2)); // duck,elephant
+console.log(animals.slice(2,-1)); // camel,duck
