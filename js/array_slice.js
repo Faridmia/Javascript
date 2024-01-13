7d3deff678861f6368d70c8cb3a0a6935ffab97c
faridmia <mdfarid7830@gmail.com>
2024-01-13 12:37:25 +0600

    slice in different way

--- js/array_slice.js
+++ js/array_slice.js
@@ -6,3 +6,20 @@ console.log(animals.slice(2)); // 2 number position thake start hobe and element
 console.log(animals.slice(2,4)); // camel,duck 
 console.log(animals.slice(-2)); // duck,elephant
 console.log(animals.slice(2,-1)); // camel,duck
+
+
+// Using slice, create newCar from myCar.
+const myHonda = {
+    color: "red",
+    wheels: 4,
+    engine: { cylinders: 4, size: 2.2 },
+};
+const myCar = [myHonda, 2, "cherry condition", "purchased 1997"];
+
+console.log(myCar[0]['engine']['cylinders']);
+const newCar = myCar.slice(0, 2);
+
+console.log(newCar);
+
+console.log("myCar[0].color=" , myCar[0].color);
+console.log("myCar[0].color=" , newCar[0].color);
