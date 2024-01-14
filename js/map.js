ccd157181fa1ab282bf8665ab978d9d7224a44f0
faridmia <mdfarid7830@gmail.com>
2024-01-14 20:03:47 +0600

    map foreach

--- js/map.js
+++ js/map.js
@@ -11,3 +11,17 @@ fruits.set("mango",300);
 
 
 console.log(fruits.get('apple'));
+
+
+const fruits2 = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+]);
+
+text2 = "";
+fruits2.forEach(function(value,key) {
+    text2 += key + "=" + value + "\n";
+});
+
+console.log(text2);
