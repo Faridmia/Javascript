b1a4997e6b234402f04dce09ddaa3502124d52c4
faridmia <mdfarid7830@gmail.com>
2024-01-14 15:40:08 +0600

    literable object

--- js/break.js
+++ js/break.js
@@ -13,3 +13,33 @@ for(i = 0;i<10;i++) {
     }
     console.log("The Number is: ", i + "\n");
 }
+
+
+const name = "W3Schools";
+
+
+for (const x of name) {
+    console.log(x + "\n");
+}
+
+const letters = ["a","b","c"];
+textt = "";
+for(const x of letters ) {
+    textt += x + "\n"; 
+}
+
+console.log(textt + "\n");
+
+
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+]);
+  
+let text3 = "";
+for(const x of fruits ) {
+    text3 += x + "\n";
+}
+
+console.log(text3);
