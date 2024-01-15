edeb5bd75534da54ad30ebd0f2fe20fa6a7d6a6c
faridmia <mdfarid7830@gmail.com>
2024-01-15 23:15:40 +0600

    converts number to string

--- js/type_conversion.js
+++ js/type_conversion.js
@@ -4,3 +4,14 @@ Number("    ") + "\n" + // 0
 Number("") + "\n" + // 0
 Number("99 88") + "\n" +  //NaN
 Number("John") + "\n"); // NaN
+
+let y = "5";
+let xy = + y; // string convert to number
+
+console.log(xy);
+
+let x = 123;
+
+console.log(x.toString() + "\n" +
+   (123).toString() + "\n" +
+   (100 + 23).toString());
