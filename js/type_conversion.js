4c887c9ac2a8128f8faeaab9169c9374c98f3fcd
faridmia <mdfarid7830@gmail.com>
2024-01-15 23:01:20 +0600

    type conversion

---
+++ js/type_conversion.js
@@ -0,0 +1,6 @@
+console.log(Number("3.14") + "\n" +  
+Number(Math.PI) + "\n" +
+Number("    ") + "\n" + // 0
+Number("") + "\n" + // 0
+Number("99 88") + "\n" +  //NaN
+Number("John") + "\n"); // NaN
