c9e33320fbbe55c48627e5a8626dba657dad7128
faridmia <mdfarid7830@gmail.com>
2024-01-16 00:32:15 +0600

    conversion

--- js/type_conversion.js
+++ js/type_conversion.js
@@ -15,3 +15,28 @@ let x = 123;
 console.log(x.toString() + "\n" +
    (123).toString() + "\n" +
    (100 + 23).toString());
+
+
+console.log( 5 + null ); // 5
+console.log("5" + null ); // 5null
+
+console.log( "5" - 2 );
+
+console.log("5" * "3");
+
+console.log(Number("twenty")); // string text k number a convert kora jai na
+
+console.log(Number([20 ]));
+
+let num2 = { 
+    firstname: "farid",
+    lastname: 'mia'
+}
+
+console.log(Number(num2));
+console.log(String(num2));
+
+let undi = undefined;
+
+// console.log(Number(undi));
+console.log(String(undi));
