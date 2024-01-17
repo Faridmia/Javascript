3b02a412ee0200e11e6bac64a18745fbc4111768
faridmia <mdfarid7830@gmail.com>
2024-01-17 22:24:48 +0600

    const hosting

--- js/javascript_hosted.js
+++ js/javascript_hosted.js
@@ -1,4 +1,11 @@
 console.log(x);
 x = 5;
-console.log(x);
+console.log(x);  // var hosted hoi abong opore chole gea x ar value undefined set kore dei but let and const use korle hosted thik e hoi but x ar man undefined set kore na abong mememory te set hoi na ai karone reference error ase
 var x;
+
+
+carName = "volvo";
+
+const carName;
+
+console.log(carName); // const sobsomoi ak line a likte hoi opore ba niche alada kore declare korle syntex error asa
