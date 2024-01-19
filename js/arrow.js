009cddbaf93a28a18f84cf1a4597080476e99678
faridmia <mdfarid7830@gmail.com>
2024-01-19 22:48:28 +0600

    arrow add even listner

--- js/arrow.js
+++ js/arrow.js
@@ -13,3 +13,26 @@ let parameter2 = "";
 parameter2 = value => "Hello " + value;
 
 console.log(parameter2("bangladesh"));
+
+
+hello3 = function() {
+    document.getElementById("demo").innerHTML += this;
+}
+
+window.addEventListener("load", hello3);
+
+document.getElementById('btn').addEventListener('click',hello3);
+
+
+
+// Arrow Function:
+hello4 = () => {
+    document.getElementById("demo2").innerHTML += this;
+  }
+  
+  // The window object calls the function:
+  window.addEventListener("load", hello4);
+  
+  // A button object calls the function:
+  document.getElementById("btn2").addEventListener("click", hello4);
+  
