d00566ae633f81f09269fb866f9f57c45b01f9e9
faridmia <mdfarid7830@gmail.com>
2024-01-20 00:14:08 +0600

    javascipt classes

---
+++ js/js-classes.js
@@ -0,0 +1,11 @@
+class Car {
+    constructor(name,year){
+        this.name = name;
+        this.year = year;
+    }
+}
+
+let obj = new Car("BMW", "2017");
+
+console.log(obj.name);
+console.log(obj.year);
