56af4e5562a53abd2aeb59421b7932bad5689669
faridmia <mdfarid7830@gmail.com>
2024-01-20 11:25:32 +0600

    class method

--- js/js-classes.js
+++ js/js-classes.js
@@ -7,5 +7,26 @@ class Car {
 
 let obj = new Car("BMW", "2017");
 
-console.log(obj.name);
-console.log(obj.year);
+// console.log(obj.name);
+// console.log(obj.year);
+
+
+
+class Car2 {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+
+    age() {
+        const currentdate = new Date();
+
+        return currentdate.getFullYear - this.year;
+    }
+    
+
+}
+
+const obj2 = new Car2("BMW", 2017);
+
+console.log("my car age is " + obj2.age() + "years old");
