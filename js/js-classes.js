7cf584ad693a70322aed6dfe6280c1a5bc056ad8
faridmia <mdfarid7830@gmail.com>
2024-01-20 11:36:32 +0600

    object method parenthesis

--- js/js-classes.js
+++ js/js-classes.js
@@ -30,3 +30,25 @@ class Car2 {
 const obj2 = new Car2("BMW", 2017);
 
 console.log("my car age is " + obj2.age() + "years old");
+
+
+class Car3 {
+    constructor(name,year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    age(x) {
+        return x - this.year;
+    }
+
+
+}
+
+const currentdate = new Date();
+
+let year = currentdate.getFullYear();
+
+const obj3 = new Car3("ahmed",2015);
+
+console.log( "my age is " + obj3.age(year)); 
