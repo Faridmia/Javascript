7cf9d0d5271ed7d730caa9da914d6b364765d9bb
faridmia <mdfarid7830@gmail.com>
2024-01-21 21:47:44 +0600

    case switch

---
+++ js/mistake.js
@@ -0,0 +1,17 @@
+let x = 10;
+
+switch (x) {
+    case 10: 
+    console.log("hello world");
+    break;
+    default: 
+    console.log("variable not working");
+}
+
+switch (x) {
+    case "10": 
+    console.log("hello world");
+    break;
+    default: 
+    console.log("variable not working");
+}
