46c17d99092b8163396a06a7ff2a42ffadb53a1f
faridmia <mdfarid7830@gmail.com>
2024-01-21 22:17:46 +0600

    array use numbered index

--- js/mistake.js
+++ js/mistake.js
@@ -28,3 +28,14 @@ let x3 = "Hello \
 World!";
 
 console.log(x3);
+
+const person = [];
+person[0] = "John";
+person[1] = "Doe";
+person[2] = 46; 
+
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+console.log(person.length);
+
