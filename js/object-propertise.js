7efdd90db4fb2afb545d76ac37e79b4233ca73ca
faridmia <mdfarid7830@gmail.com>
2024-02-04 22:36:21 +0600

    nested javascript objectarray

--- js/object-propertise.js
+++ js/object-propertise.js
@@ -17,3 +17,44 @@ for( let x in  obj_propertise) {
 console.log(txt);
 
 
+let myObj = {
+    name:"John",
+    age:30,
+    cars: {
+      car1:"Ford",
+      car2:"BMW",
+      car3:"Fiat"
+    }
+}
+
+
+let obj_v1 = myObj.cars.car1; 
+let obj_v2 = myObj.cars['car2']; 
+let obj_v3 = myObj.cars['car3']; 
+
+console.log(obj_v1);
+console.log(obj_v2);
+console.log(obj_v3);
+
+
+let x2 = "";
+const myObj2 = {
+  name: "John",
+  age: 30,
+  cars: [
+    {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+    {name:"BMW", models:["320", "X3", "X5"]},
+    {name:"Fiat", models:["500", "Panda"]}
+  ]
+}
+
+
+for(let i in myObj2.cars ) {
+    x2 += myObj2.cars[i].name + "\n";
+
+    for(let j in myObj2.cars[i].models ) {
+        x2 += myObj2.cars[i].models[j] + "\n";
+    }
+}
+
+console.log(x2);
