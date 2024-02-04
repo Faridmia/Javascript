fd3ff935f6a4da9dce3725b16c3ba43e28aafb9f
faridmia <mdfarid7830@gmail.com>
2024-02-04 22:12:57 +0600

    delete property object

--- js/object-propertise.js
+++ js/object-propertise.js
@@ -6,13 +6,13 @@ const obj_propertise = {
 
 obj_propertise.nationality = "English";
 
+delete obj_propertise.age;
+
 let txt = "";
 for( let x in  obj_propertise) {
      txt += obj_propertise[x]+"\n";
      
 }
-
-
 
 console.log(txt);
 
