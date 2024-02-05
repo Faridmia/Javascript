afa751fa6ba4a01390f6b961eda4901ac187dd1b
faridmia <mdfarid7830@gmail.com>
2024-02-05 21:58:30 +0600

    object method

---
+++ js/object-method.js
@@ -0,0 +1,11 @@
+// Create an object:
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+console.log( person.fullName() );
