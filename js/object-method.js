6220e81415715e1ce5c2c1628d37a160c11029fb
faridmia <mdfarid7830@gmail.com>
2024-02-05 22:33:43 +0600

    person object

--- js/object-method.js
+++ js/object-method.js
@@ -9,3 +9,16 @@ const person = {
 };
 
 console.log( person.fullName() );
+
+const person2 = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+};
+
+person2.name = function() {
+    return this.firstName + " " + this.lastName;
+}
+
+console.log("My name is " + person2.name());
+
