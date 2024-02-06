56ac3aeadcd66a1b3c208d036756773f6ed73732
faridmia <mdfarid7830@gmail.com>
2024-02-06 21:52:04 +0600

    object accories

---
+++ js/object-accessors.js
@@ -0,0 +1,11 @@
+// Create an object:
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    language: "en",
+    get lang() {
+      return this.language;
+    }
+};
+
+console.log(person.lang);
