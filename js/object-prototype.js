5d61b054d8a0de03b11909704ece3da3a0e10d10
faridmia <mdfarid7830@gmail.com>
2024-02-07 22:19:29 +0600

    object prototype method add

--- js/object-prototype.js
+++ js/object-prototype.js
@@ -9,3 +9,20 @@ Person.prototype.country = "Bangladesh";
 let obj = new Person("farid","mia",30,"blue");
 
 console.log("my name is" + " " + obj.firstName + "and my nationality " + obj.country);
+
+
+function Person2(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+
+let obj2 = new Person2("hasan","sheikh",30,"blue");
+
+Person2.prototype.name = function() {
+    return "My name is " + this.firstName + " " + this.lastName;
+}
+
+console.log(obj2.name());
+  
