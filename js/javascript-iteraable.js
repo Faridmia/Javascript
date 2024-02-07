ac6a41f4bdd52367de401650c8d50547b405e45c
faridmia <mdfarid7830@gmail.com>
2024-02-07 23:11:20 +0600

    symbol iterator

--- js/javascript-iteraable.js
+++ js/javascript-iteraable.js
@@ -15,3 +15,32 @@ m.next();
 m.next();
 
 console.log(m.next().value);
+
+// Create an Object
+let myNumbers2 = {};
+
+
+// make it iterable
+
+myNumbers2[Symbol.iterator] = function () {
+    let n = 0;
+    done = false;
+    
+    return {
+        next() {
+            n += 10;
+            if( n == 100 ) {
+                done = true;
+            }
+
+            return {value:n,done:done}
+        }
+    }
+}
+
+for(const num of  myNumbers2 ) {
+    console.log(num);
+}
+
+
+
