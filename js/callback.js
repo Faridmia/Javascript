da0069d583f46f2105a1498f8ebbfc58264190cc
faridmia <mdfarid7830@gmail.com>
2024-02-16 15:53:36 +0600

    callback

--- js/callback.js
+++ js/callback.js
@@ -1,13 +1,15 @@
-function display(some) {
-    console.log(some);
-}
+// function display(some) {
+//     console.log(some);
+// }
 
 function calculator(num1,num2,callback) {
     let sum = num1 + num2;
     callback(sum);
-    //return sum;
+   // return sum;
 }
 
-let obj = calculator(10,15, display);
+calculator(10,15, function(result){
+    console.log(result)
+});
 
 //display(obj);
