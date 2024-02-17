fca9576a94bdf31c440f67985b1c8e702259846c
faridmia <mdfarid7830@gmail.com>
2024-02-17 11:54:44 +0600

    dom css

---
+++ dom/part2.js
@@ -0,0 +1,12 @@
+let ele = document.querySelector("#demo");
+
+ele.style.color = "red";
+ele.style.fontSize = "40px";
+
+
+function myFunction(){
+    let ele = document.querySelector("#demo5");
+
+    ele.style.color = "red";
+    ele.style.fontSize = "40px";
+}
