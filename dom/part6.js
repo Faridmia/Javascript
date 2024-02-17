3ddad6157cf91ea649eb0b59ec949ec111de0e54
faridmia <mdfarid7830@gmail.com>
2024-02-17 22:04:05 +0600

    remove child

--- dom/part6.js
+++ dom/part6.js
@@ -3,4 +3,13 @@ const node = document.createTextNode("This is new.");
 para.appendChild(node);
 
 const element = document.getElementById("div1");
-element.appendChild(para);
+// element.appendChild(para);
+
+const child = document.getElementById("p1");
+
+element.insertBefore(para,child);
+
+const parent = document.getElementById("div1");
+
+
+parent.removeChild(child);
