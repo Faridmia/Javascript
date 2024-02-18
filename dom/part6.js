2ecd1a01601e6f0af871ae0046c4fd4800178377
faridmia <mdfarid7830@gmail.com>
2024-02-18 15:58:59 +0600

    dom project

--- dom/part6.js
+++ dom/part6.js
@@ -11,5 +11,16 @@ element.insertBefore(para,child);
 
 const parent = document.getElementById("div1");
 
+parent.replaceChild(para, child);
 
-parent.removeChild(child);
+
+// parent.removeChild(child);
+
+
+
+function myFunction() {
+    const myCollection = document.getElementsByTagName("p");
+    for (let i = 0; i < myCollection.length; i++) {
+      myCollection[i].style.color = "red";
+    }
+  }
