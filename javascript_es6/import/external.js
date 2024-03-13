93b5101a37b6adaf5fd8c74c412d17aea82416e2
faridmia <mdfarid7830@gmail.com>
2024-03-13 22:36:39 +0600

    javascript import export

---
+++ javascript_es6/import/external.js
@@ -0,0 +1,9 @@
+export const pi = 3.14;
+
+export const test = "hello world";
+
+// export default test;
+
+export default function Myfunc() {
+    console.log("hello function data");
+}
---
+++ javascript_es6/import/playground.js
@@ -0,0 +1,8 @@
+// import {pi,test} from "./external.js";
+import * as data from "./external.js";
+import external, {pi,test} from "./external.js";
+
+// console.log(data.pi)
+console.log(pi,test);
+
+external();
