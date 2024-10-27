611216dfe4ef20ca956e703b5f48df1f8af412ea
faridmia <mdfarid7830@gmail.com>
2024-10-27 20:39:40 +0600

    array splice

--- array-method/part1.js
+++ array-method/part1.js
@@ -9,3 +9,13 @@ console.log(arr.slice( -1 )); // e
 console.log(arr.slice( 1, -2 )); // b, c
 console.log(arr.slice()); // array ful copy hobe
 console.log(arr.slice( [...arr] )); // array ful copy hobe
+
+
+// splice method
+let arr2 = ['a','b','c','d','e'];
+arr2.splice(-1); 
+console.log(arr2); // ['a','b','c','d']
+
+arr2.splice( 1, 2 ); 
+
+console.log( arr2 ); // ['a','d','e']; 
