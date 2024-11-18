e790b155e73aae90468dbe205f2ad288412e8bc8
faridmia <mdfarid7830@gmail.com>
2024-11-18 08:13:19 +0600

    array method which use

---
+++ array-method/part13.js
@@ -0,0 +1,97 @@
+// to mutate original array
+
+// add to original
+
+1. .push()
+2. .unshift()
+
+// remove from original
+
+.pop()
+
+.shift()
+
+.splice()
+
+// others
+
+.reverse()
+
+.sort()
+.fill();
+
+// second step
+
+// new array
+// computed from original
+.map();
+
+// filtered using condition
+
+.filter();
+
+// portion of original
+
+.slice();
+
+// adding original to other
+
+.contat()
+
+// flattening the original
+
+.flat()
+
+.flatMap();
+
+
+// step 3
+// An array index
+// based on value
+
+.indexOf();
+
+// based on test condition
+
+.findIndex();
+
+// an array element
+// based on test condition
+
+.find();
+
+// step four
+
+// know if array inclues
+
+// based on value
+
+.inclues();
+
+// based on test condition
+
+.some();
+.every();
+
+
+// A new string
+
+/// based on separator string
+
+.join();
+
+
+// step four
+
+// to transform to value
+
+// based on accumulator
+
+.reduce();
+
+// to just loop array
+
+.forEach(); // based on callback
+
+// does not creat a new array,
+
