33ac1ef40d3fbd402cf293608834c326c0c96c74
faridmia <mdfarid7830@gmail.com>
2024-11-23 21:03:41 +0600

    split lowercase and join function

--- 10-Functions/final/script.js
+++ 10-Functions/final/script.js
@@ -44,57 +44,79 @@
 // const closureFunc = outerFunction("Outside");
 // closureFunc("Inside");
 
-function firstFunction() {
-  secondFunction();
-}
+// function firstFunction() {
+//   secondFunction();
+// }
 
-function secondFunction() {
-  thirdFunction();
-}
+// function secondFunction() {
+//   thirdFunction();
+// }
 
-function thirdFunction() {
+// function thirdFunction() {
   
-  //debugger; 
-  console.log("Inside third function");
-}
+//   //debugger; 
+//   console.log("Inside third function");
+// }
 
 
-firstFunction();
+// firstFunction();
 
 
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+
+// const flight = 'LH234';
+
+// const jonas = {
+//     name: 'Jonas Schmedtmann',
+//     passport: 24739479284,
+// };
+
+
+// const checkIn = function( flightNum, passenger ) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 24739479284) {
+//     //alert('Checked in');
+//   } else {
+//     //alert('Wrong passport!');
+//   }
+// }
+
+// const newPassport = function(person) {
+//   person.passport = Math.trunc( Math.random() * 100000000000);
+// };
+
+// newPassport(jonas);
 
 // checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
 
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
-
-
-const flight = 'LH234';
-
-const jonas = {
-    name: 'Jonas Schmedtmann',
-    passport: 24739479284,
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
+const upperFirstWord = function( str ) {
+    const [first, ...others ] = str.split(' ');
 
-const checkIn = function( flightNum, passenger ) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
-
-  if (passenger.passport === 24739479284) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport!');
-  }
+    return [ first.toUpperCase(), ...others].join(' ');
 }
 
-const newPassport = function(person) {
-  person.passport = Math.trunc( Math.random() * 100000000000);
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
 };
 
-newPassport(jonas);
-
-checkIn(flight, jonas);
+transformer('bangladesh is a small country!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
