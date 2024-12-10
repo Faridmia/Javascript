a2fe98ae181f633944c0f52d085b18da43f3469d
faridmia <mdfarid7830@gmail.com>
2024-12-10 08:55:07 +0600

    optional chaning

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -396,3 +396,50 @@ let result = arr.reduce((acc, cur) => {
 }, []);
 
 console.log(result.length);
+
+
+// with optional chaning
+
+if( restaurant.openingHours && restaurant.openingHours.mon )
+  console.log(restaurant.openingHours.mon.open);
+
+
+console.log(restaurant.openingHours.thu?.open);
+console.log(restaurant.openingHours?.thu?.open);
+
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for( const day of days ) {
+    const open = restaurant.openingHours[day]?.open ?? 'closed';
+    console.log(`On ${day}, we open at ${open}`);
+}
+
+// method
+
+console.log( restaurant.order?.(0,1) ?? 'Method does not exist..');
+
+console.log( restaurant.orderRisotto?.( 0,1 ) ?? 'Method does not exist..');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+
+console.log( users[0]?.name ?? "user array emtpy");
+
+if( users.length > 0 ) console.log(users[0].name);
+else console.log('user array empty..');
+
+
+// the for loop
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu ];
+
+for( const item of menu ) console.log( item );
+
+for ( const [i, el ] of menu.entries() ) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+console.log([...menu.entries() ]);
