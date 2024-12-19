f73806fd6ecbbc1901fb65ab6491e3fb493ddc35
faridmia <mdfarid7830@gmail.com>
2024-12-19 08:22:46 +0600

    javascript hoisting

--- 08-Behind-the-Scenes/final/script.js
+++ 08-Behind-the-Scenes/final/script.js
@@ -131,3 +131,74 @@ const jonas = {
 };
 jonas.greet();
 jonas.calcAge();
+
+console.log("this keyword function");
+
+// The this Keyword in Practice
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(1980);
+
+// const jonas33 = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// jonas33.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas33.calcAge;
+// matilda.calcAge();
+
+// const f = jonas33.calcAge;
+// f();
+
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow);
+//console.log(addArrow(2, 7));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// // Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
