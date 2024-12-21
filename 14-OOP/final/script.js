da14607d5ac5d1c2b87a08d8755c7795f36ccb97
faridmia <mdfarid7830@gmail.com>
2024-12-21 20:28:20 +0600

    gatter and setter

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -164,3 +164,25 @@ jessica.greet();
 
 const walter = new PersonCl('Walter White', 1965);
 PersonCl.hey(); // static method only class thake e call kora jai but object thake call kora jai na
+
+
+// getter and setter
+
+const account = {
+  owner: 'Jonas',
+  movements: [200,300, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  }
+}
+
+console.log( account.latest );
+
+account.latest = 50;
+
+console.log(account.movements);
