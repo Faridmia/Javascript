e928dae5d353be5c80fed39fcbaa67d35bc40bc2
faridmia <mdfarid7830@gmail.com>
2024-12-21 16:30:07 +0600

    static method

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -107,44 +107,60 @@ bmw.accelerate();
 // const PersonCl = class {}
 
 // Class declaration
+
+
 class PersonCl {
-  constructor(fullName, birthYear) {
+
+  constructor( fullName, birthYear ) {
     this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
-  // Instance methods
-  // Methods will be added to .prototype property
   calcAge() {
-    console.log(2037 - this.birthYear);
-  }
-
-  greet() {
-    console.log(`Hey ${this.fullName}`);
+    console.log( 2037 - this.birthYear );
   }
 
   get age() {
     return 2037 - this.birthYear;
   }
 
-  // Set a property that already exists
+  greet() {
+    console.log( `Hey ${this.fullName}`);
+  }
+
   set fullName(name) {
-    if (name.includes(' ')) this._fullName = name;
+    if(name.includes(" ")) this._fullName = name;
     else alert(`${name} is not a full name!`);
   }
 
   get fullName() {
-    return this._fullName;
+    return this._fullName; 
   }
 
-  // Static method
   static hey() {
     console.log('Hey there 👋');
     console.log(this);
   }
 }
 
-const jessica = new PersonCl('Jessica Davis', 1996);
+const jessica = new PersonCl("Farid Mia", 1997 );
 console.log(jessica);
 jessica.calcAge();
-console.log(jessica.age);
+console.log( jessica.age );
+
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function() {
+//     console.log(`Hey ${this.firstName}`);
+// };
+
+PersonCl.prototype.greet = function() {
+  const firstName = this.fullName.split(' ')[0];
+  console.log(`Hey ${firstName}`);
+};
+
+jessica.greet();
+
+const walter = new PersonCl('Walter White', 1965);
+PersonCl.hey(); // static method only class thake e call kora jai but object thake call kora jai na
