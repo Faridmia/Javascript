4c77f16db8273ed8610ad0751b9a64d8793473ee
faridmia <mdfarid7830@gmail.com>
2025-01-01 23:39:47 +0600

    javascript instanceof

--- 14-OOP/final/script.js
+++ 14-OOP/final/script.js
@@ -258,3 +258,42 @@ ford.accelerate();
 ford.brake();
 ford.speedUS = 50;
 console.log(ford);
+
+
+const  Person2 = function( firstName, birthYear ) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+}
+
+Person2.prototype.calcAge = function() {
+  console.log( 2037 - this.birthYear );
+}
+
+// linking prototype 
+
+const Student = function( firstName, birthYear, course ) {
+    Person2.call( this, firstName, birthYear );
+    this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+
+Student.prototype.introduce = function() {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
